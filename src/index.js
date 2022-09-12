31ed05c500e11a8f9c5d2b3246f87c75841c49f9
Rashmi Aribenchi <raribenchi@bunnings.com.au>
2022-09-13 01:55:08 +0800

    Updated CSS & JS

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ forecastElement.innerHTML = forecastHTML;
  }
 
 function getForecast(coordinates) {
-    console.log(coordinates);
+    //console.log(coordinates);
     let apiKey = "49e5da6325b173bba5c08dae7a5eba79";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 //console.log(apiUrl);
